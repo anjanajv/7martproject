c906a7f8a659d00f5ac1c02da74b338a7aa1845b
anjan <anjan@LAPTOP-2S8BC3KU>
2023-06-06 17:15:21 +0530

    first push

---
+++ 7MartSuperMarket/src/test/java/obsqura/MartSuperMarket/Base.java
@@ -0,0 +1,25 @@
+package obsqura.MartSuperMarket;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class Base {
+ public WebDriver driver;
+ @BeforeMethod
+ public void intializeBrowser() {
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\anjan\\eclipse-workspace\\SeleniumTestingFramework\\src\\main\\java\\Resources\\chromedriver.exe");
+	 ChromeOptions chromeOptions = new ChromeOptions();
+	 chromeOptions.addArguments("--remote-allow-origins=*");
+	 driver = new ChromeDriver(chromeOptions);
+	 driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+ }
+ @AfterMethod
+ public void driverQuit() {
+	   driver.quit();
+ }
+}
